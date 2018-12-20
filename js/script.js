381a794fc46c016ac6f34866d07f05a1debee7fb
DakitaNorth <garryant3120@yandex.ru>
2018-12-20 17:46:45 +0300

    Список изменений:
    Исправил описку в html(пиксели в размерах)
    Поправил работу попапа
    Добавил анимации к попапу

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 var link  = document.querySelector(".close_open_button");
 var popup = document.querySelector(".modal_window");
-var search = document.querySelector(".search_button");
+var arrival_data = popup.querySelector("[name=arrival_data]");
+var departure_data = popup.querySelector("[name=departure_data]");
+var grownup_form = popup.querySelector("[name=grownup]");
+var child_form = popup.querySelector("[name=child]");
+var search = popup.querySelector(".search_button");
+
 
 link.addEventListener("click",function(evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal_close")) {
-      popup.classList.remove("modal_close");
-      popup.classList.add("modal_show");
+      popup.classList.toggle("modal_close");
     }
     else {
-      popup.classList.remove("modal_show");
-      popup.classList.add("modal_close");
+      popup.classList.toggle("modal_close");
     }
 });
+
+search.addEventListener("click", function (evt) {
+    if (!arrival_data.value || !departure_data.value || !grownup_form.value || !child_form.value) {
+      evt.preventDefault();
+      search.classList.remove("search_button_shake");
+      search.offsetWidth = search.offsetWidth;
+      search.classList.add("search_button_shake");
+    }
+  });
